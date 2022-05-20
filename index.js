2876c5baa83c13273b3722b705b193977fd02a36
Freshy <79219288+Frshy@users.noreply.github.com>
2022-05-20 18:07:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  nie trzeba sie uczyc
-// @author       You
+// @author       Freshy
 // @match        *://www.eduelo.pl/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=eduelo.pl
 // ==/UserScript==
